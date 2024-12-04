1b95e8b3f4a745952c653b241f358be6e6ab3c58
Denny Rubino <rubidenny@gmail.com>
2024-12-05 00:44:01 +0100

    fix fuction

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -9,9 +9,10 @@ const word = prompt("Inserisci una parola");
 //Creò una funzione che : divida la parola in lettere, la inverta e la rimetta insieme
 function control(word) {
 
- const wordPalindrome = word.split("").reverse().join("");
+ const wordPalindrome = word.split('').reverse('').join('');
  return word === wordPalindrome;
 }
 //controllo se funziona la funzione
 //console.log(control("anna"));
+
 
