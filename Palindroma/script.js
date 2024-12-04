2cbd70a2d50377e13147eaa7291f5e2d13b7552e
Denny Rubino <rubidenny@gmail.com>
2024-12-04 23:47:11 +0100

    add ask prompt

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -2,4 +2,6 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
+//Chiedo la parola all'utente
+const word = prompt("Inserisci una parola");
 
